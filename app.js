9c599a2e9236746ce187e1d599132d4d654cfccc
Rahmat Noris Adityabrillian <rahmatnoris@gmail.com>
2020-10-16 11:08:16 +0700

    [Adit] admin dashboard integration

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// Router Admin
+const adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -18,24 +20,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    '/sb-admin-2',
+    express.static(
+        path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')
+    )
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// Admin
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
