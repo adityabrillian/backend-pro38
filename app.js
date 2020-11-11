bd750fb3a816c9483413ad81ba5281ef9753c330
Rahmat Noris Adityabrillian <rahmatnoris@gmail.com>
2020-11-11 22:19:45 +0700

    [adit] install cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors')
 // Import Mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://aditnoris:backendpro38@cluster0.lmf93.mongodb.net/pro38?retryWrites=true&w=majority', {
@@ -48,6 +49,7 @@ app.use(
   express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))
 );
 
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // Admin
